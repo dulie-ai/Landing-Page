a3f9d5c82f10446121b09683ace7391b6a0d3c98
Yu Ke Mi <yukemi@KMs-MacBook-Air.local>
2026-09-10 19:00:20 +0800

    Deploy landing page automatically with GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import process from "node:process";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
 });
